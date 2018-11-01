913afd9dbfd005f9e979ffc89a62f0cb6890fa4c
xfeldman <x.feldman@gmail.com>
2018-11-01 01:36:59 +0100

    d3 animation added

--- src/main/kotlin/D3Sample.kt
+++ src/main/kotlin/D3Sample.kt
@@ -1,9 +1,11 @@
+import d3.selection.BaseType
+import d3.selection.Selection
 import d3.selection.select
 import org.w3c.dom.HTMLDivElement
-import org.w3c.dom.svg.SVGCircleElement
-import org.w3c.dom.svg.SVGElement
-import org.w3c.dom.svg.SVGGeometryElement
-import org.w3c.dom.svg.SVGRectElement
+import org.w3c.dom.svg.*
+import kotlin.coroutines.experimental.*
+import kotlinx.coroutines.experimental.launch
+import org.w3c.dom.HTMLElement
 
 data class MyDatum(val number: Double)
 
@@ -12,22 +14,15 @@ fun d3sample() {
             .append<SVGElement>("svg")
             .attr("width", 100)
             .attr("height", 100)
-    drawCircle()
+    drawCircle(true)
 }
 
-fun animate(datum:MyDatum, index:Number, groups:Array<SVGCircleElement>) {
-    select<SVGCircleElement, MyDatum>(groups[index.toInt()]).transition()
-            .attr("r", 10)
-            .transition()
-            .delay(1000)
-            .attr("r", 40)
-}
-
-fun drawCircle() {
+fun drawCircle(visible: Boolean):Selection<out SVGCircleElement, MyDatum, HTMLElement, Any> {
     var svg = select<HTMLDivElement, MyDatum>("#d3Sample").select<SVGElement>("svg")
-    svg.append<SVGCircleElement>("circle")
+    return svg.append<SVGCircleElement>("circle")
             .style("stroke", "gray")
             .style("fill", "white")
+            .style("opacity", if (visible) 1 else 0)
             .attr("id", "myShape")
             .attr("r", 40)
             .attr("cx", 40)
@@ -40,11 +35,12 @@ fun drawCircle() {
             .on("mouseup", ::replaceWithCircle)
 }
 
-fun drawRect() {
+fun drawRect(visible:Boolean):Selection<out SVGRectElement, MyDatum, HTMLElement, Any> {
     var svg = select<HTMLDivElement, MyDatum>("#d3Sample").select<SVGElement>("svg")
-    svg.append<SVGRectElement>("rect")
+    return svg.append<SVGRectElement>("rect")
             .style("stroke", "gray")
             .style("fill", "white")
+            .style("opacity", if (visible) 1 else 0)
             .attr("id", "myShape")
             .attr("width", 80)
             .attr("height", 80)
@@ -56,13 +52,48 @@ fun drawRect() {
             .on("mouseup", ::replaceWithCircle)
 }
 
-fun replaceWithRect(datum: MyDatum, index:Number, groups:Array<out SVGGeometryElement>) {
-    select<SVGCircleElement, MyDatum>("#myShape").remove()
-    drawRect()
+fun replaceWithRect(datum: MyDatum, index:Number, groups:Array<out BaseType>) {
+    val shape = select<SVGCircleElement, MyDatum>("#myShape")
+    launch {
+        animateDown(shape)
+        shape.remove()
+        val rect = drawRect(false)
+        animateUp(rect)
+    }
 }
 
-fun replaceWithCircle(datum: MyDatum, index:Number, groups:Array<out SVGGeometryElement>) {
-    select<SVGCircleElement, MyDatum>("#myShape").remove()
-    drawCircle()
+fun replaceWithCircle(datum: MyDatum, index:Number, groups:Array<out BaseType>) {
+    val shape = select<SVGCircleElement, MyDatum>("#myShape")
+    launch {
+        animateDown(shape)
+        shape.remove()
+        val rect = drawCircle(false)
+        animateUp(rect)
+    }
 }
 
+suspend fun animateDown(selection: Selection<out BaseType, MyDatum, HTMLElement, Any>):Selection<out BaseType, MyDatum, HTMLElement, Any> =
+    suspendCoroutine { cont ->
+        selection
+                .style("opacity", 1)
+                .transition()
+                .delay(0)
+                .duration(200)
+                .style("opacity", 0)
+                .on("end") {datum:MyDatum, index:Number, groups:Array<out BaseType> ->
+                    cont.resume(selection)
+                }
+    }
+
+suspend fun animateUp(selection: Selection<out BaseType, MyDatum, HTMLElement, Any>):Selection<out BaseType, MyDatum, HTMLElement, Any> =
+    suspendCoroutine { cont ->
+        selection
+                .style("opacity", 0)
+                .transition()
+                .delay(0)
+                .duration(200)
+                .style("opacity", 1)
+                .on("end") {datum:MyDatum, index:Number, groups:Array<out BaseType> ->
+                    cont.resume(selection)
+                }
+    }
